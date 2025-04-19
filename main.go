a8140b4ba0e9910ef8072937591bfd14f4ea67d9
shubhamku044 <shubhamku044@gmail.com>
2025-04-19 13:41:18 +0530

    add containix to gitignore

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-        "os"
-        "os/exec"
-        "path/filepath"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
-        // This is a wrapper that calls the actual application in cmd/containix
-        // This allows for backward compatibility while using the new structure
-        cmd := exec.Command(filepath.Join("cmd", "containix", "containix"))
-        cmd.Stdin = os.Stdin
-        cmd.Stdout = os.Stdout
-        cmd.Stderr = os.Stderr
-        if err := cmd.Run(); err != nil {
-                panic(err)
-        }
+	// This is a wrapper that calls the actual application in cmd/containix
+	// This allows for backward compatibility while using the new structure
+	cmd := exec.Command(filepath.Join("cmd", "containix", "containix"))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
 }
